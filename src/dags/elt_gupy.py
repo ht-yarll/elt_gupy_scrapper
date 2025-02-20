29ba373726d720408fea59b1c2ef88f1ed5e7aa3
ht-yarll <h.scott.xp@gmail.com>
2025-02-20 14:26:17 -0300

    :construction:

--- src/dags/elt_gupy.py
+++ src/dags/elt_gupy.py
@@ -5,11 +5,20 @@ This dag scrape data from an URL and load it to a bucket on storage for further 
 
 from airflow import Dataset
 from airflow.decorators import dag, task
-from pendulum import datetime
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
 import pandas as pd
 import requests
+from datetime import datetime
 
 # config = load_config()
+
+default_args = {
+    "owner": "astro",
+    "depends_on_past": False,
+    "retries": 3
+}
 
 @dag(
     dag_id = "elt_gupy",
@@ -17,14 +26,14 @@ import requests
     schedule = "@daily",
     catchup = False,
     doc_md = __doc__,
-    default_args = {"owner": "ht-yarll", "retries": 3},
+    default_args = default_args,
     tags = ["elt"]
 )
 def elt_gupy():
 #Execution --------------------------------------------------------------------------
 
     @task
-    def extract():
+    def extract() -> pd.DataFrame:
         offset = 0
         all_data = []
         label = 'python'
@@ -49,6 +58,7 @@ def elt_gupy():
             
             result = pd.DataFrame(all_data)
             print('All data fetched with success')
+            print (result)
             return result
            
         except Exception as e:
@@ -57,32 +67,63 @@ def elt_gupy():
         
     @task
     def normalize_data():
-        df = extract()
+        dataframe = extract()
         try:
             print('Treating data...')
-            df.columns = (
-                df.columns
+            dataframe.columns = (
+                dataframe.columns
                 .str.strip()
                 .str.lower()
                 .str.replace(r"[^a-z0-9_]", "_", regex=True)
 
             )
-            df = df.map(
+            dataframe = dataframe.map(
             lambda x: str(x) if isinstance(x, dict) else x
         )
-            df = df.fillna({
+            dataframe = dataframe.fillna({
                 col: "N/A" if df[col].dtype == "object" else 0
                 for col in df.columns
             })
-            df = df.drop_duplicates()
+            df = dataframe.drop_duplicates()
             df = df.convert_dtypes()
             
         except Exception as e:
             print(f'Error during treating data: {e}')
 
         return df
-
-
-    extract() >> normalize_data()
     
-elt_gupy()    
+    @task
+    def check_if_file_exists():
+        # task to check if exist file in bkt
+        check_file = GCSObjectExistenceSensor(
+            task_id='check_file_exists',
+            bucket='elt_gupy_scrapper',  # Replace with your bucket name
+            object='all_data.parquet',  # Replace with the file path in the bucket
+            timeout=300,  # Maximum wait time in seconds
+            poke_interval=30,  # Time interval in seconds to check again
+            mode='poke',  # Use 'poke' mode for synchronous checking
+        )
+
+    @task
+    def upload_file_to_gcs():
+        # task to upload file to bq
+        upload_file = GCSToBigQueryOperator(
+                task_id='load_parquet_to_bq',
+                bucket='elt_gupy_scrapper',  # Replace with your bucket name
+                source_objects=['all_data.parquet'],  # Path to your file in the bucket
+                destination_project_dataset_table='blackstone-446301.elt_gupy_scrapper.all_data_raw',  # Replace with your project, dataset, and table name
+                source_format='Parquet', 
+                allow_jagged_rows=True,
+                ignore_unknown_values=True,
+                write_disposition='WRITE_APPEND',  # Options: WRITE_TRUNCATE, WRITE_APPEND, WRITE_EMPTY
+                skip_leading_rows=1,  # Skip header row
+                autodetect=True,  # Automatically infer schema from the file
+                #google_cloud_storage_conn_id='google_cloud_default',  # Uncomment and replace if custom GCP connection
+                #bigquery_conn_id='google_cloud_default',  # Uncomment and replace if custom BigQuery connection
+           
+        )
+
+
+    [extract() >> normalize_data()] >> check_if_file_exists() >> upload_file_to_gcs()
+
+elt_gupy()
