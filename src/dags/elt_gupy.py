9c5b8fa2d2a06df1031548bf954ff9d7a4825a6e
ht-yarll <h.scott.xp@gmail.com>
2025-02-20 01:08:14 -0300

    :construction: building extract code task

--- src/dags/elt_gupy.py
+++ src/dags/elt_gupy.py
@@ -1,24 +1,88 @@
 """
 This dag scrape data from an URL and load it to a bucket on storage for further transformation
 """
+# from utils.load_config import load_config
 
 from airflow import Dataset
 from airflow.decorators import dag, task
 from pendulum import datetime
+import pandas as pd
 import requests
+
+# config = load_config()
 
 @dag(
     dag_id = "elt_gupy",
     start_date = datetime(2025, 2, 18),
     schedule = "@daily",
-    catchup = False
+    catchup = False,
+    doc_md = __doc__,
+    default_args = {"owner": "ht-yarll", "retries": 3},
     tags = ["elt"]
 )
 def elt_gupy():
-#Execução --------------------------------------------------------------------------
+#Execution --------------------------------------------------------------------------
 
-@task
-def extract():
-    url = f"https://portal.api.gupy.io/api/job?name={label}&offset={offset}&limit=400"
-    r = requests.get(url)
-    return r
+    @task
+    def extract():
+        offset = 0
+        all_data = []
+        label = 'python'
+        print(f'Fetching data for {label}...')
+        try:
+            while True:
+                url_template = (
+                    f"https://portal.api.gupy.io/api/job?name={label}&offset={offset}&limit=400"
+                    )
+                print(f'Fetching page {offset}...')
+
+                response = requests.get(url_template)
+                data = response.json()
+                
+                for i in data['data']:
+                    all_data.append(i)
+
+                if not data['data']:
+                    break
+
+                offset += 10
+            
+            result = pd.DataFrame(all_data)
+            print('All data fetched with success')
+            return result
+           
+        except Exception as e:
+            print(f'Failed to fetch data: {e}')
+            return pd.DataFrame()
+        
+    @task
+    def normalize_data():
+        df = extract()
+        try:
+            print('Treating data...')
+            df.columns = (
+                df.columns
+                .str.strip()
+                .str.lower()
+                .str.replace(r"[^a-z0-9_]", "_", regex=True)
+
+            )
+            df = df.map(
+            lambda x: str(x) if isinstance(x, dict) else x
+        )
+            df = df.fillna({
+                col: "N/A" if df[col].dtype == "object" else 0
+                for col in df.columns
+            })
+            df = df.drop_duplicates()
+            df = df.convert_dtypes()
+            
+        except Exception as e:
+            print(f'Error during treating data: {e}')
+
+        return df
+
+
+    extract() >> normalize_data()
+    
+elt_gupy()    
--- src/dags/utils/load_config.py
+++ src/dags/utils/load_config.py
@@ -1,10 +1,9 @@
-from typing import Dict, Any
-import io
+# from typing import Dict, Any
 
-import yaml
+# import yaml
 
-def load_config() -> Dict[str, Any]:
-    with open("config.yaml", "r") as file:
-        config = yaml.safe_load(file)
+# def load_config() -> Dict[str, Any]:
+#     with open('config.yaml', "r") as file:
+#         config = yaml.safe_load(file)
 
-    return config
+#     return config
